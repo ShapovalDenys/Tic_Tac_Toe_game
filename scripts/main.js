5b632dbd9b7b1e37c3e95ed950f04ac72c184007
ShapovalDenys <denys.igorevich@gmail.com>
2020-04-21 14:06:58 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const game = document.querySelector('.game');
+const message = document.querySelector('.alert');
+const winnerO = document.querySelector('.winnerO');
+const winnerX = document.querySelector('.winnerX');
+const winnerXO = document.querySelector('.winnerXO');
+const button = document.querySelector('.button');
 
 for (let i = 0; i < 9; i++) {
   game.innerHTML += `<div class='block'></div>`;
@@ -38,8 +43,10 @@ function checkWinner() {
   || (arrBlock[2].innerHTML === 'x' && arrBlock[5].innerHTML === 'x' && arrBlock[8].innerHTML === 'x')
   || (arrBlock[0].innerHTML === 'x' && arrBlock[4].innerHTML === 'x' && arrBlock[8].innerHTML === 'x')
   || (arrBlock[2].innerHTML === 'x' && arrBlock[4].innerHTML === 'x' && arrBlock[6].innerHTML === 'x')) {
-    alert('Виграли Крестики');
-    reset();
+    message.style.display = 'block';
+    winnerX.style.display = 'block';
+    game.style.opacity = 0.4;
+    game.style['z-index'] = -1;
   }
 
   if ((arrBlock[0].innerHTML === 'o' && arrBlock[1].innerHTML === 'o' && arrBlock[2].innerHTML === 'o')
@@ -50,15 +57,21 @@ function checkWinner() {
   || (arrBlock[2].innerHTML === 'o' && arrBlock[5].innerHTML === 'o' && arrBlock[8].innerHTML === 'o')
   || (arrBlock[0].innerHTML === 'o' && arrBlock[4].innerHTML === 'o' && arrBlock[8].innerHTML === 'o')
   || (arrBlock[2].innerHTML === 'o' && arrBlock[4].innerHTML === 'o' && arrBlock[6].innerHTML === 'o')) {
-    alert('Виграли Нулики');
-    reset();
+    message.style.display = 'block';
+    winnerO.style.display = 'block';
+    game.style.opacity = 0.4;
+    game.style['z-index'] = -1;
   }
 
   if (numberOfMoves === 9) {
-    setTimeout(() => alert('Ніхто не виграв'), 100);
-    setTimeout(reset, 150);
+    message.style.display = 'block';
+    winnerXO.style.display = 'block';
+    game.style.opacity = 0.4;
+    game.style['z-index'] = -1;
   }
 }
+
+button.addEventListener('click', reset);
 
 function reset() {
   window.location.reload();
